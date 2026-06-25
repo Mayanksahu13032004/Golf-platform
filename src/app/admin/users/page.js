7e8c8bf3bd15985597ae8b19a7a63a7f540e156b
Hansrajsahu10 <hansrajsahuhansrajsahu10@gmail.com>
2026-06-26 00:16:34 +0530

    done

---
+++ src/app/admin/users/page.js
@@ -0,0 +1,11 @@
+"use client";
+
+export default function UsersPage() {
+  return (
+    <div className="p-6 text-white">
+      <h1 className="text-4xl font-bold">
+        Users
+      </h1>
+    </div>
+  );
+}
